0b938c7cf5ce4b3f6e792dec8739731535b67103
mysend12 <mysend12@naver.com>
2022-08-22 17:32:18 +0900

    feat: Add figma's components (#1)

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,158 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class ColorConst {
+  Color secondaryLight = const Color(0xffFFB703);
+  Color secondaryDark = const Color(0xffFB8501);
+
+  Color black = const Color(0xff000000);
+  Color darkGray = const Color(0xff4F4F4F);
+  Color gray = const Color(0xffBDBDBD);
+  Color lightGray = const Color(0xffE7E7E7);
+
+  Color getPrimary();
+  Color getPrimaryLight();
+  Color getPrimaryDark();
+  Color getPrimaryPlus();
+
+  Color getSecondaryLight() {
+    return secondaryLight;
+  }
+
+  Color getSecondaryDark() {
+    return secondaryDark;
+  }
+
+  Color getBlack() {
+    return black;
+  }
+
+  Color getDarkGray() {
+    return darkGray;
+  }
+
+  Color getGray() {
+    return gray;
+  }
+
+  Color getLightGray() {
+    return lightGray;
+  }
+}
+
+class OceanMainColor extends ColorConst {
+  Color primary = const Color(0xff219EBC);
+  Color primaryLight = const Color(0xff64CFEF);
+  Color primaryDark = const Color(0xff065B83);
+  Color primaryPlus = const Color(0xffF6FDFF);
+
+  @override
+  Color getPrimary() {
+    return primary;
+  }
+
+  @override
+  Color getPrimaryDark() {
+    return primaryDark;
+  }
+
+  @override
+  Color getPrimaryLight() {
+    return primaryLight;
+  }
+
+  @override
+  Color getPrimaryPlus() {
+    return primaryPlus;
+  }
+
+}
+
+class YellowMainColor extends ColorConst {
+  Color primary = const Color(0xffFFE55A);
+  Color primaryLight = const Color(0xffEFE4AD);
+  Color primaryDark = const Color(0xffFFB703);
+
+  // TODO 컬러 수정
+  Color primaryPlus = const Color(0xff);
+
+  @override
+  Color getPrimary() {
+    return primary;
+  }
+
+  @override
+  Color getPrimaryDark() {
+    return primaryDark;
+  }
+
+  @override
+  Color getPrimaryLight() {
+    return primaryLight;
+  }
+
+  @override
+  Color getPrimaryPlus() {
+    return primaryPlus;
+  }
+}
+
+class PurpleMainColor extends ColorConst {
+  Color primary = const Color(0xff9747FF);
+  Color primaryLight = const Color(0xffD3B1FF);
+  Color primaryDark = const Color(0xff6012C5);
+
+  // TODO 컬러 수정
+  Color primaryPlus = const Color(0xff);
+
+  @override
+  Color getPrimary() {
+    return primary;
+  }
+
+  @override
+  Color getPrimaryDark() {
+    return primaryDark;
+  }
+
+  @override
+  Color getPrimaryLight() {
+    return primaryLight;
+  }
+
+  @override
+  Color getPrimaryPlus() {
+    return primaryPlus;
+  }
+}
+
+class OrangeMainColor extends ColorConst {
+  Color primary = const Color(0xffFB8501);
+  Color primaryLight = const Color(0xffFFB703);
+  Color primaryDark = const Color(0xffF4DEA7);
+
+  // TODO 컬러 수정
+  Color primaryPlus = const Color(0xff);
+
+  @override
+  Color getPrimary() {
+    return primary;
+  }
+
+  @override
+  Color getPrimaryDark() {
+    return primaryDark;
+  }
+
+  @override
+  Color getPrimaryLight() {
+    return primaryLight;
+  }
+
+  @override
+  Color getPrimaryPlus() {
+    return primaryPlus;
+  }
+}
+
---
+++ lib/constants/constants_text_style.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/widgets.dart';
+
+const TextStyle kHeadline6 = TextStyle(
+  height: 1.448,
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -1.5,
+);
+
+const TextStyle kSubtitle1 = TextStyle(
+  height: 1.5,
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.15,
+);
+
+const TextStyle kSubtitle2 = TextStyle(
+  height: 1.714,
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.1,
+);
+
+const TextStyle kBody1 = TextStyle(
+  height: 1.75,
+  fontSize: 16,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0.44,
+);
+
+const TextStyle kBody2 = TextStyle(
+  height: 1.428,
+  fontSize: 14,
+  fontWeight: FontWeight.w100,
+  letterSpacing: 0.25,
+);
+
+const TextStyle kButton = TextStyle(
+  height: 1.448,
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 1.35,
+);
+
+const TextStyle kCaption = TextStyle(
+  height: 1.33,
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.4,
+);
+
+const TextStyle kOverLine = TextStyle(
+  height: 1.6,
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 1.5,
+);
+
