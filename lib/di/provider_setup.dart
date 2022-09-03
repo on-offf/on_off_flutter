4dd21b7b4bb6b422f0a556fefd968d65ae6ce4fa
mysend12 <mysend12@naver.com>
2022-09-03 15:55:59 +0900

    feature: add provider for off_screens

--- lib/di/provider_setup.dart
+++ lib/di/provider_setup.dart
@@ -4,6 +4,10 @@ import 'package:on_off/data/data_source/db/off/off_image_dao.dart';
 import 'package:on_off/domain/use_case/data_source/off_diary_use_case.dart';
 import 'package:on_off/domain/use_case/data_source/off_icon_use_case.dart';
 import 'package:on_off/domain/use_case/data_source/off_image_use_case.dart';
+import 'package:on_off/ui/off/detail/off_detail_view_model.dart';
+import 'package:on_off/ui/off/home/off_home_view_model.dart';
+import 'package:on_off/ui/off/list/off_list_view_model.dart';
+import 'package:on_off/ui/off/write/off_write_view_model.dart';
 import 'package:on_off/ui/setting/setting_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
@@ -32,11 +36,16 @@ Future<List<SingleChildWidget>> getProviders() async {
   OffImageUseCase offImageUseCase = OffImageUseCase(offImageDAO);
   OffIconUseCase offIconUseCase = OffIconUseCase(offIconDAO);
 
-  
-
-
+  OffHomeViewModel offHomeViewModel = OffHomeViewModel();
+  OffWriteViewModel offWriteViewModel = OffWriteViewModel();
+  OffListViewModel offListViewModel = OffListViewModel();
+  OffDetailViewModel offDetailViewModel = OffDetailViewModel();
 
   return [
     ChangeNotifierProvider(create: (_) => SettingViewModel()),
+    ChangeNotifierProvider(create: (_) => offWriteViewModel),
+    ChangeNotifierProvider(create: (_) => offHomeViewModel),
+    ChangeNotifierProvider(create: (_) => offListViewModel),
+    ChangeNotifierProvider(create: (_) => offDetailViewModel),
   ];
 }
