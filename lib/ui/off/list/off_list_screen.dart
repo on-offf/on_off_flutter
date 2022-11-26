01bf6367324fd53402007dab41b388da485e9593
mysend12 <mysend12@naver.com>
2022-11-26 20:04:36 +0900

    feature: remove list page's empty bottom space(#72)

--- lib/ui/off/list/off_list_screen.dart
+++ lib/ui/off/list/off_list_screen.dart
@@ -22,148 +22,146 @@ class OffListScreen extends StatelessWidget {
     UiProvider uiProvider = context.watch<UiProvider>();
 
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.only(
-            left: 37,
-            right: 37,
-          ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 80,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FocusMonth(
-                      isAccent: true,
-                    ),
-                  ],
-                ),
+      body: Container(
+        padding: const EdgeInsets.only(
+          left: 37,
+          right: 37,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FocusMonth(
+                    isAccent: true,
+                  ),
+                ],
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height - 173,
-                child: ListView.builder(
-                  itemBuilder: ((context, index) {
-                    if (index == 0 && viewModel.state.contents.isEmpty) {
-                      return Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: const [
-                              OffListOrderChangeButton(),
-                            ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height - 90,
+              child: ListView.builder(
+                itemBuilder: ((context, index) {
+                  if (index == 0 && viewModel.state.contents.isEmpty) {
+                    return Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            OffListOrderChangeButton(),
+                          ],
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height / 5,
+                        ),
+                        Image(
+                          image: AssetImage(IconPath.noHaveContent.name),
+                          width: 130,
+                          height: 130,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          '이번 달은 아직 \n게시글이 없습니다!',
+                          style: kSubtitle3.copyWith(
+                            color: uiProvider.state.colorConst.getPrimary(),
                           ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height / 5,
-                          ),
-                          Image(
-                            image: AssetImage(IconPath.noHaveContent.name),
-                            width: 130,
-                            height: 130,
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            '이번 달은 아직 \n게시글이 없습니다!',
-                            style: kSubtitle3.copyWith(
-                              color: uiProvider.state.colorConst.getPrimary(),
+                          textAlign: TextAlign.center,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                uiProvider.changeFloatingActionButtonSwitch(true);
+                                Navigator.pushNamed(
+                                    context, OffWriteScreen.routeName);
+                              },
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                      18.0,
+                                    ),
+                                    side: BorderSide(
+                                      color: uiProvider.state.colorConst.getPrimary(),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              child: const Text(
+                                '글쓰러가기',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 14,
+                                  letterSpacing: 0.1,
+                                ),
+                              ),
                             ),
-                            textAlign: TextAlign.center,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  uiProvider.changeFloatingActionButtonSwitch(true);
-                                  Navigator.pushNamed(
-                                      context, OffWriteScreen.routeName);
-                                },
-                                style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                        18.0,
-                                      ),
-                                      side: BorderSide(
-                                        color: uiProvider.state.colorConst.getPrimary(),
-                                      ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                uiProvider.changeFloatingActionButtonSwitch(true);
+                                Navigator.pop(context);
+                              },
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                      18.0,
+                                    ),
+                                    side: BorderSide(
+                                      color: uiProvider.state.colorConst.getPrimary(),
                                     ),
                                   ),
                                 ),
-                                child: const Text(
-                                  '글쓰러가기',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 14,
-                                    letterSpacing: 0.1,
-                                  ),
+                              ),
+                              child: const Text(
+                                '뒤로 가기',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 14,
+                                  letterSpacing: 0.1,
                                 ),
                               ),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  uiProvider.changeFloatingActionButtonSwitch(true);
-                                  Navigator.pop(context);
-                                },
-                                style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(
-                                        18.0,
-                                      ),
-                                      side: BorderSide(
-                                        color: uiProvider.state.colorConst.getPrimary(),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                child: const Text(
-                                  '뒤로 가기',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 14,
-                                    letterSpacing: 0.1,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      );
-                    } else if (index == 0) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: const [
-                          OffListOrderChangeButton(),
-                        ],
-                      );
-                    } else {
-                      return GestureDetector(
-                        child: ListItem(content: viewModel.state.contents[index - 1]),
-                        onTap: () {
-                          //daily 스크린으로 이동
-                          uiProvider.changeSelectedDay(
-                              viewModel.state.contents[index - 1].time);
-                          uiProvider.changeFocusedDay(
-                              viewModel.state.contents[index - 1].time);
-                          Navigator.pushNamed(
-                              context, OffDailyScreen.routeName);
-                        },
-                      );
-                    }
-                  }),
-                  itemCount: viewModel.state.contents.length + 1,
-                ),
+                            )
+                          ],
+                        ),
+                      ],
+                    );
+                  } else if (index == 0) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        OffListOrderChangeButton(),
+                      ],
+                    );
+                  } else {
+                    return GestureDetector(
+                      child: ListItem(content: viewModel.state.contents[index - 1]),
+                      onTap: () {
+                        //daily 스크린으로 이동
+                        uiProvider.changeSelectedDay(
+                            viewModel.state.contents[index - 1].time);
+                        uiProvider.changeFocusedDay(
+                            viewModel.state.contents[index - 1].time);
+                        Navigator.pushNamed(
+                            context, OffDailyScreen.routeName);
+                      },
+                    );
+                  }
+                }),
+                itemCount: viewModel.state.contents.length + 1,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
