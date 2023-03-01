cd812ed2a423387749566b684c32baef1d4bc146
mysend12 <mysend12@naver.com>
2023-03-01 14:24:23 +0900

    fix: on 캘린더의 TO-DO 목록 영역 수정
    ON 캘린더에서 TO-DO 목록 영역 하단의 빈 공간이 너무 많아 이상해보이는 현상을 수정.
    1. 현재 위치를 알기 위해 각 아이템들을 컴포넌트화
    2. 위치를 동적으로 변경하는 코드 추가
    3. 초기 화면에 접속했을때, 비동기 코드로 인해 데이터가 없다는 에러가 발생하는 현상 수정

--- lib/ui/on/monthly/components/on_monthly_item.dart
+++ lib/ui/on/monthly/components/on_monthly_item.dart
@@ -1,24 +1,16 @@
-import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:on_off/constants/constants_text_style.dart';
-import 'package:on_off/domain/entity/on/on_todo.dart';
 import 'package:on_off/domain/icon/icon_path.dart';
+import 'package:on_off/ui/on/monthly/components/on_todo_components.dart';
 import 'package:on_off/ui/on/monthly/components/todo_bottom_sheet.dart';
 import 'package:on_off/ui/on/monthly/on_monthly_view_model.dart';
 import 'package:on_off/ui/provider/ui_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 class OnMonthlyItem extends StatelessWidget {
-  OnMonthlyItem({super.key});
-
-  final ScrollController _scrollController = ScrollController();
-  final ScrollController _scrollController2 = ScrollController();
-
-  var createTodoTextFormFieldController = TextEditingController();
+  OnMonthlyItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,58 +18,6 @@ class OnMonthlyItem extends StatelessWidget {
     UiProvider uiProvider = context.watch<UiProvider>();
     LayerLink layerLink = LayerLink();
 
-    _scrollControllerListener(_scrollController, uiProvider);
-    _scrollControllerListener(_scrollController2, uiProvider);
-
-    return Container(
-      decoration: uiProvider.state.calendarFormat == CalendarFormat.week
-          ? const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(22),
-                topRight: Radius.circular(22),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(-3, -3),
-                  blurRadius: 1,
-                  spreadRadius: 1,
-                  color: Color.fromRGBO(0, 0, 0, .1),
-                ),
-              ],
-            )
-          : const BoxDecoration(),
-      child: Container(
-        decoration: uiProvider.state.calendarFormat == CalendarFormat.week
-            ? BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(22),
-                  topRight: Radius.circular(22),
-                ),
-                color: uiProvider.state.colorConst.canvas,
-              )
-            : const BoxDecoration(),
-        padding: EdgeInsets.only(
-          top: uiProvider.state.calendarFormat == CalendarFormat.month ? 0 : 25,
-          left: 37,
-          right: 37,
-        ),
-        child: SingleChildScrollView(
-          physics: uiProvider.state.calendarFormat == CalendarFormat.month
-              ? const NeverScrollableScrollPhysics()
-              : const BouncingScrollPhysics(),
-          controller: _scrollController,
-          child: bottomView(context, viewModel, uiProvider, layerLink),
-        ),
-      ),
-    );
-  }
-
-  Widget bottomView(
-    context,
-    OnMonthlyViewModel viewModel,
-    UiProvider uiProvider,
-    layerLink,
-  ) {
     return Column(
       children: [
         Row(
@@ -101,242 +41,56 @@ class OnMonthlyItem extends StatelessWidget {
               onPressed: () => _buildBottomSheet(context, uiProvider),
               icon: SvgPicture.asset(
                 IconPath.menu.name,
-                color: uiProvider.state.colorConst.getPrimary(),
               ),
               iconSize: 5,
             ),
           ],
         ),
-        SizedBox(
-          height: 500,
-          child: Column(
-            children: [
-              SingleChildScrollView(
-                physics: uiProvider.state.calendarFormat == CalendarFormat.month
-                    ? const NeverScrollableScrollPhysics() //아래에서 위로 스와이프하지 않았을 때 스크롤 방지
-                    : const BouncingScrollPhysics(),
-                controller: _scrollController2,
-                child: items(context, viewModel, uiProvider),
-              ),
-              if (viewModel.state.multiDeleteStatus)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        viewModel.updateMultiDeleteStatus();
-                      },
-                      child: Container(
-                        height: 40,
-                        width: 200,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey[500],
-                        ),
-                        child: const Text('취소'),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        viewModel.deleteMultiOnTodo();
-                      },
-                      child: Container(
-                        height: 40,
-                        width: 200,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey[500],
-                        ),
-                        child: const Text('전체 삭제'),
-                      ),
-                    ),
-                  ],
-                ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget items(
-    context,
-    OnMonthlyViewModel viewModel,
-    uiProvider,
-  ) {
-    return Column(
-      children: [
-        const SizedBox(height: 15),
-        DottedBorder(
-          borderType: BorderType.RRect,
-          radius: const Radius.circular(7),
-          color: uiProvider.state.colorConst.getPrimary(),
-          strokeWidth: 1,
-          child: Container(
-            height: 50,
-            color: const Color(0xfff8f8f8),
-            child: Row(
-              children: [
-                const SizedBox(
-                  height: 50,
-                  width: 40,
-                ),
-                Expanded(
-                  child: TextFormField(
-                    style: kSubtitle2.copyWith(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                    controller: createTodoTextFormFieldController,
-                    decoration: InputDecoration(
-                      hintText: "오늘의 리스트를 추가해 주세요!",
-                      hintStyle:
-                          kSubtitle2.copyWith(fontWeight: FontWeight.bold),
-                      enabledBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: uiProvider.state.colorConst.getPrimary(),
-                        ),
-                      ),
-                    ),
-                    onFieldSubmitted: (value) async {
-                      await viewModel.saveContent(value);
-                      createTodoTextFormFieldController.text = '';
+        Column(
+          children: [
+            OnTodoComponents(),
+            if (viewModel.state.multiDeleteStatus)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      viewModel.updateMultiDeleteStatus();
                     },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        const SizedBox(height: 10),
-        SizedBox(
-          height: 300,
-          child: ReorderableListView.builder(
-            buildDefaultDragHandles: viewModel.state.order == 'todoOrder',
-            itemBuilder: (context, index) {
-              return buildTodo(
-                viewModel.state.todos![index],
-                viewModel,
-                uiProvider,
-              );
-            },
-            itemCount: viewModel.state.todos!.length,
-            onReorder: (oldIndex, newIndex) async {
-              List<OnTodo> copyTodos = [];
-              int i = 0;
-              for (int index = 0;
-                  index < viewModel.state.todos!.length;
-                  index++) {
-                if (index == oldIndex) {
-                  continue;
-                } else if (index == newIndex) {
-                  OnTodo todo = viewModel.state.todos![oldIndex];
-                  copyTodos.add(todo.copyWith(todoOrder: i++));
-                }
-
-                OnTodo todo = viewModel.state.todos![index];
-                copyTodos.add(todo.copyWith(todoOrder: i++));
-              }
-
-              if (newIndex == viewModel.state.todos!.length) {
-                OnTodo todo = viewModel.state.todos![oldIndex];
-                copyTodos.add(todo.copyWith(todoOrder: newIndex));
-              }
-              await viewModel.updateTodos(copyTodos);
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-  buildTodo(
-    OnTodo todo,
-    OnMonthlyViewModel viewModel,
-    UiProvider uiProvider,
-  ) {
-    return Column(
-      key: ObjectKey(todo.id),
-      children: [
-        SizedBox(
-          height: 40,
-          child: Row(
-            children: [
-              if (viewModel.state.multiDeleteStatus)
-                Checkbox(
-                  value:
-                      viewModel.state.multiDeleteTodoIds.containsKey(todo.id),
-                  onChanged: (isChecked) async {
-                    await viewModel.updateMultiDeleteTodoIdCheck(
-                        todo.id!, isChecked!);
-                  },
-                  checkColor: const Color(0xffFFFFFF),
-                ),
-              Expanded(
-                child: Slidable(
-                  endActionPane: ActionPane(
-                    extentRatio: .15,
-                    motion: const ScrollMotion(),
-                    children: [
-                      IconButton(
-                        onPressed: () async {
-                          await viewModel.deleteTodo(todo);
-                        },
-                        icon: SvgPicture.asset(
-                          width: 18,
-                          height: 18,
-                          IconPath.trashCan.name,
-                        ),
+                    child: Container(
+                      height: 40,
+                      width: 200,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey[500],
                       ),
-                    ],
+                      child: const Text('취소'),
+                    ),
                   ),
-                  child: Row(
-                    children: [
-                      Checkbox(
-                        value: todo.status == 1 ? true : false,
-                        onChanged: (bool? value) async {
-                          await viewModel.changeTodoStatus(todo);
-                        },
-                        activeColor: uiProvider.state.colorConst.getPrimary(),
-                        side: const BorderSide(
-                          color: Color(0xffD9D9D9),
-                          width: 1,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
+                  GestureDetector(
+                    onTap: () {
+                      viewModel.deleteMultiOnTodo();
+                    },
+                    child: Container(
+                      height: 40,
+                      width: 200,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey[500],
                       ),
-                      Text(
-                        todo.title,
-                        style: todo.status == 1
-                            ? kBody2.copyWith(
-                                color: const Color(0xffb3b3b3),
-                                decoration: TextDecoration.lineThrough,
-                              )
-                            : kBody2,
-                      ),
-                    ],
+                      child: const Text('전체 삭제'),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
+          ],
         ),
       ],
     );
   }
 
-  void _scrollControllerListener(
-      ScrollController scrollController, UiProvider uiProvider) {
-    scrollController.addListener(() {
-      if (scrollController.offset < -50) {
-        uiProvider.changeCalendarFormat(CalendarFormat.month);
-      }
-    });
-  }
 
   Future<dynamic> _buildBottomSheet(
       BuildContext context, UiProvider uiProvider) {
